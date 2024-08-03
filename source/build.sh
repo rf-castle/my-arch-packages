9e0033c015dd113387bbd398c60d1354306b7401
rf-castle <49747813+rf-castle@users.noreply.github.com>
2024-08-03 17:32:20 +0900

    ほい

--- source/build.sh
+++ source/build.sh
@@ -7,8 +7,9 @@ set -e
 # paruのインストール(依存パッケージを持ってきながらビルドするため用)
 # Todo: 毎回インストールするのは非効率なので、どっかから持ってくる
 git clone https://aur.archlinux.org/paru.git
-cd paru
+pushd paru
 makepkg -si --noconfirm
+popd
 
 # 対象パッケージをビルド
 cd $1
