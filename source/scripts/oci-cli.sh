33692d1ef777bfa5cfba064420569e0f1902063b
rf-castle <49747813+rf-castle@users.noreply.github.com>
2025-07-19 22:56:59 +0900

    よくよく考えたら単独でよくねぇか

--- source/scripts/oci-cli.sh
+++ source/scripts/oci-cli.sh
@@ -6,5 +6,3 @@ set -e
 
 # パッケージのアップデート
 sudo -u builder -- paru -S --noconfirm python-terminaltables3
-sudo -u builder -- mkdir -p ${PACKAGE_PATH}/dist
-mv /var/cache/pacman/pkg/*.pkg.tar.zst ${PACKAGE_PATH}/dist
