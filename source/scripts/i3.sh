bc98fae91c39956a242c7964800a25b05c1e88b6
rf-castle <49747813+rf-castle@users.noreply.github.com>
2025-12-23 02:46:44 +0900

    スクリプト修正

--- source/scripts/i3.sh
+++ source/scripts/i3.sh
@@ -4,23 +4,29 @@
 # スクリプトをエラーで停止させる
 set -e
 
-# # patchのコピー
-# cp "$(dirname "$0")/i3.patch" $PACKAGE_PATH
+# あらかじめperlを入れておかないとパスがうまく設定されなさそうなので
+# ここで一度インストールしておく
+if ! pacman -Q perl &>/dev/null; then
+    sudo pacman -S --noconfirm perl
+fi
 
-# # PKGBUILDを編集する
-# pushd $PACKAGE_PATH
+# patchのコピー
+cp "$(dirname "$0")/i3.patch" $PACKAGE_PATH
 
-# # source行
-# # 複数行に渡るため、perlを使って行を追加
-# perl -0777 -i -pe '
-#   s/(^source=\([\s\S]*?)(\))/\1 "i3.patch"\2/m
-# ' PKGBUILD
-# # i3.patchのb2sums行を追加
-# perl -i -pe '
-#   s/(^b2sums=\(.*?)(\))/\1 \n                    '\''4e48e9cd9d83f269ea023f4e6b5104250498757f0af3d72496cb47a5aa60aeb9f0757751e302580e44daca92f7a9843b6cc66c2c53454fef9a871d1a05437bc3'\''\2/
-# ' PKGBUILD
+# PKGBUILDを編集する
+pushd $PACKAGE_PATH
 
-# # PKGBUILDのpackage()のcd i3の直後にパッチ適用行を追加
-# sed -i '/^package() {$/,/^}$/ s/cd i3$/&\n  patch -Np1 < ..\/i3.patch/' PKGBUILD
+# source行
+# 複数行に渡るため、perlを使って行を追加
+perl -0777 -i -pe '
+  s/(^source=\([\s\S]*?)(\))/\1 "i3.patch"\2/m
+' PKGBUILD
+# i3.patchのb2sums行を追加
+perl -i -pe '
+  s/(^b2sums=\(.*?)(\))/\1 \n                    '\''4e48e9cd9d83f269ea023f4e6b5104250498757f0af3d72496cb47a5aa60aeb9f0757751e302580e44daca92f7a9843b6cc66c2c53454fef9a871d1a05437bc3'\''\2/
+' PKGBUILD
 
-# popd
+# PKGBUILDのpackage()のcd i3の直後にパッチ適用行を追加
+sed -i '/^package() {$/,/^}$/ s/cd i3$/&\n  patch -Np1 < ..\/i3.patch/' PKGBUILD
+
+popd
